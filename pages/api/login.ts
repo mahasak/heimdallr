c1def34729fac65928a1cec40c7ac2fd85885fa3
Max Pijittum <mpijittum@fb.com>
2021-07-27 22:17:06 +0800

    remove log from login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -6,8 +6,7 @@ export default async function registerUser(req, res) {
     email: email,
     password: password,
   });
-  console.log({ error });
-  console.log({ user });
+  
   if (error) return res.status(401).json({ error: error.message });
   return res.status(200).json({ user: user });
 }
