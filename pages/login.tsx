e7505c54f4693bd22940fd78ff27a0e7748732a1
Max Pijittum <mpijittum@fb.com>
2021-07-27 22:23:04 +0800

    redirect after login

--- pages/login.tsx
+++ pages/login.tsx
@@ -30,7 +30,7 @@ const Login = () => {
       if (event === 'USER_UPDATED') setTimeout(() => setAuthView('sign_in'), 1000)
       // Send session to /api/auth route to set the auth cookie.
       // NOTE: this is only needed if you're doing SSR (getServerSideProps)!
-      
+
       fetch('/api/auth', {
         method: 'POST',
         headers: new Headers({ 'Content-Type': 'application/json' }),
@@ -64,8 +64,8 @@ const Login = () => {
           />
         </Space>
       )
-      Router.push('/protected')
-      
+    Router.push('/protected')
+        
     return (
       <Space direction="vertical" size={6}>
         {authView === 'forgotten_password' && <Auth.UpdatePassword supabaseClient={supabase} />}
@@ -73,34 +73,8 @@ const Login = () => {
           <>
             <Typography.Text>You're signed in</Typography.Text>
             <Typography.Text strong>Email: {user.email}</Typography.Text>
-
-            <Button
-              icon={<Icon type="LogOut" />}
-              type="outline"
-              onClick={() => supabase.auth.signOut()}
-            >
-              Log out
-            </Button>
             {error && <Typography.Text type="danger">Failed to fetch user!</Typography.Text>}
-            {data && !error ? (
-              <>
-                <Typography.Text type="success">
-                  User data retrieved server-side (in API route):
-                </Typography.Text>
-
-                <Typography.Text>
-                  <pre>{JSON.stringify(data, null, 2)}</pre>
-                </Typography.Text>
-              </>
-            ) : (
-              <div>Loading...</div>
-            )}
-
-            <Typography.Text>
-              <Link href="/protected">
-                <a>SSR example with getServerSideProps</a>
-              </Link>
-            </Typography.Text>
+            <div>Redirecting...</div>
           </>
         )}
       </Space>
